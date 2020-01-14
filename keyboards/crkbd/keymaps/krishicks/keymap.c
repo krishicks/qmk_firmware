e18a519a1faa34e739238b33b35812537225ff2c
Kris Hicks <krishicks@gmail.com>
2020-01-13 22:51:15 -0800

    Set lower space to tab

--- keyboards/crkbd/keymaps/krishicks/keymap.c
+++ keyboards/crkbd/keymaps/krishicks/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSFT,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                         KC_LGUI,   KC_LALT,  KC_SPC,     KC_ENT,  LOWER,  RAISE \
+                                         KC_LGUI,   KC_LALT,  KC_TAB,     KC_ENT,  LOWER,  RAISE \
                                       //`--------------------------'  `--------------------------'
     ),
 
