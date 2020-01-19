adff5beca3f95455e92cc0854af654fbeb4b5938
Kris Hicks <krishicks@gmail.com>
2020-01-18 18:29:55 -0800

    Disable RGB animations to save size

--- keyboards/crkbd/keymaps/krishicks/config.h
+++ keyboards/crkbd/keymaps/krishicks/config.h
@@ -37,10 +37,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLED_NUM
-    #define RGBLIGHT_ANIMATIONS
     #define RGBLED_NUM 27
     #define RGBLIGHT_LIMIT_VAL 120
     #define RGBLIGHT_HUE_STEP 10
     #define RGBLIGHT_SAT_STEP 17
     #define RGBLIGHT_VAL_STEP 17
+
+    // Undefine RGBLIGHT_ANIMATIONS to save 4KiB
+    // #define RGBLIGHT_ANIMATIONS
 #endif
