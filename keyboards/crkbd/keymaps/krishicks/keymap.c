24a47361097ade95c57c32d85ed08f7cab0b5e44
Kris Hicks <krishicks@gmail.com>
2020-01-22 00:14:04 -0800

    Update keymap to use more natural spacebar

--- keyboards/crkbd/keymaps/krishicks/keymap.c
+++ keyboards/crkbd/keymaps/krishicks/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                         KC_LGUI,  KC_LALT,  KC_SPC,     KC_ENT,  LOWER,  RAISE \
+                                         KC_LGUI,  KC_LALT,  RAISE,     LOWER,  KC_SPC,  XXXXXXX \
                                       //`--------------------------'  `--------------------------'
 
   ),
@@ -48,23 +48,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_TAB,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_BSPC,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,  KC_MUTE, KC_VOLD, KC_VOLU, KC_PGUP, KC_PGDN,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_HOME, KC_END,\
+      KC_LCTL,  KC_MUTE, KC_VOLD, KC_VOLU, KC_PGUP, KC_PGDN,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX, XXXXXXX,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,  KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,                         KC_F6,  KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_TILD,\
+      KC_LSFT,  KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,                         KC_F6,  KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_RSFT,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                         KC_LGUI,  KC_LALT,  KC_SPC,     KC_ENT,  LOWER,  RAISE \
+                                         KC_LGUI,  KC_LALT,  RAISE,     LOWER,  XXXXXXX,  XXXXXXX \
                                       //`--------------------------'  `--------------------------'
     ),
 
   [_RAISE] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_ESC, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,                         KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,\
+      KC_ESC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MINS, KC_EQL,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,XXXXXXX, XXXXXXX,                        KC_MINS, KC_EQL, KC_LCBR, KC_RCBR, KC_PIPE, KC_GRV,\
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX,  KC_INS, KC_PAUS,                      KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS, XXXXXXX,\
+      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX,  KC_INS, KC_PAUS,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BSLS, KC_GRV,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                         KC_LGUI,  KC_LALT,  KC_SPC,     KC_ENT,  LOWER,  RAISE \
+                                         KC_LGUI,  XXXXXXX,  RAISE,     LOWER,  KC_ENT,  XXXXXXX \
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                         KC_LGUI,  KC_LALT,  KC_SPC,     KC_ENT,  LOWER,  RAISE \
+                                         KC_LGUI,  XXXXXXX,  RAISE,     LOWER,  XXXXXXX,  XXXXXXX \
                                       //`--------------------------'  `--------------------------'
   )
 };
